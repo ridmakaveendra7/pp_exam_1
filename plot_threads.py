5fc90bd7ffc91ed5fd2d4b60030b6126c74082c1
hasara-1997 <hasara@my-rate.de>
2026-01-20 12:02:42 +0100

    plot the graph

---
+++ plot_threads.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+
+threads = [1, 2, 4, 8, 16, 32, 64]
+
+print("Enter execution time for each thread count:")
+
+times = []
+
+for t in threads:
+    value = float(input(f"Time for {t} threads: "))
+    times.append(value)
+
+plot_name = input("Enter plot name/title: ")
+
+plt.figure()
+plt.plot(threads, times, marker="o")
+plt.xlabel("Thread Count")
+plt.ylabel("Execution Time")
+plt.title(plot_name)
+plt.grid(True)
+
+plt.show()
